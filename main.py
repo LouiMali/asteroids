321b48ab61316e458026370accfae5458ed95587
github_dakotabammer <louismalinowski1993@gmail.com>
2025-06-23 18:32:56 +0200

    included FPS

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ from constants import *
 
 
 def main():
-
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    while pygame.init():
+    my_clock = pygame.time.Clock()
+    dt = 0
+
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,"black")
+            
+        screen.fill("black")
+
+        # refreshing the screen
         pygame.display.flip()
-    
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+        # limit the framerate to 60 FPS
+        dt = (my_clock.tick(60) / 1000)
 
 
 if __name__ == "__main__":
