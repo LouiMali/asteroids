154c21b6316822e3aecb49b2de0f56e62691b706
github_dakotabammer <louismalinowski1993@gmail.com>
2025-06-23 17:58:16 +0200

    created .gitignore file

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ import pygame
 from constants import *
 
 
-print("Starting Asteroids!")
-print(f"Screen width: {SCREEN_WIDTH}")
-print(f"Screen height: {SCREEN_HEIGHT}")
+def main():
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while pygame.init():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,"black")
+        pygame.display.flip()
+    
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__":
     main()
